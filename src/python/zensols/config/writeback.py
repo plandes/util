a2c59ad6d88460d2db1aff2fb13b7725a824fe9f
Paul Landes <landes@mailc.net>
2020-06-15 18:07:21 -0500

    set type restrictions on writeback

--- src/python/zensols/config/writeback.py
+++ src/python/zensols/config/writeback.py
@@ -3,9 +3,11 @@
 """
 __author__ = 'Paul Landes'
 
-from typing import Set
+from typing import Set, Any
 from dataclasses import dataclass
 import logging
+from itertools import chain
+from zensols.persist import PersistedWork, PersistableContainer
 from zensols.config import Configurable, ConfigFactory
 
 logger = logging.getLogger(__name__)
@@ -16,13 +18,33 @@ class Writeback(object):
     DEFAULT_SKIP_ATTRIBUTES = set([ConfigFactory.NAME_ATTRIBUTE,
                                    ConfigFactory.CONFIG_ATTRIBUTE,
                                    ConfigFactory.CONIFG_FACTORY_ATTRIBUTE])
+    DEFAULT_ALLOW_TYPES = set([str, int, float, bool, list, tuple, dict])
     name: str
     config: Configurable
 
-    def _skip_attributes(self) -> Set[str]:
+    def _get_skip_attributes(self) -> Set[str]:
         return self.DEFAULT_SKIP_ATTRIBUTES
 
-    def __setattr__(self, name, value):
+    def _get_allow_types(self) -> Set[type]:
+        return self.DEFAULT_ALLOW_TYPES
+
+    def _is_allow_type(self, value: Any) -> bool:
+        if isinstance(value, tuple) or isinstance(value, list):
+            for i in value:
+                if not self._is_allow_type(i):
+                    return False
+            return True
+        elif isinstance(value, dict):
+            for i in chain.from_iterable(value.items()):
+                if not self._is_allow_type(i):
+                    return False
+            return True
+        return value.__class__ in self._get_allow_types()
+
+    def __setattr__(self, name: str, value: Any):
+        skip_attribs = self._get_skip_attributes()
         super().__setattr__(name, value)
-        if hasattr(self, 'config') and name not in self._skip_attributes():
+        if hasattr(self, 'config') and \
+           name not in skip_attribs and \
+           self._is_allow_type(value):
             self.config.set_option(name, value, section=self.name)
--- test/python/test_config_write.py
+++ test/python/test_config_write.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
+from typing import List, Dict
 import unittest
 import logging
 import collections
-from zensols.config import Config, ImportConfigFactory, Writeback
+from zensols.config import (
+    Config,
+    ImportConfigFactory,
+    Writeback,
+)
+from zensols.persist import PersistableContainer, persisted
 
 logger = logging.getLogger(__name__)
 
@@ -10,6 +16,21 @@ logger = logging.getLogger(__name__)
 @dataclass
 class Temp1(Writeback):
     aval: int
+    bval: bool
+    cval: float
+    dval: str
+    fval: List[str]
+    gval: Dict[str, int]
+
+
+@dataclass
+class Temp2(Writeback, PersistableContainer):
+    aval: int
+
+    @property
+    @persisted('_propval', transient=True)
+    def propval(self):
+        return 10
 
 
 class TestConfigWriteBase(unittest.TestCase):
@@ -22,7 +43,14 @@ class TestConfigWrite(TestConfigWriteBase):
     def test_primitive(self):
         config = self.config
         sec = config.get_options('temp1')
-        self.assertEqual({'aval': '1', 'class_name': 'test_config_write.Temp1'}, sec)
+        self.assertEqual({'aval': '1',
+                          'bval': 'True',
+                          'cval': '1.23',
+                          'dval': 'firstval',
+                          'fval': 'eval: [1, 2]',
+                          'gval': "eval: {'a': 3, 'b': 10}",
+                          'class_name': 'test_config_write.Temp1'},
+                         sec)
 
         empty_sec = 'empty'
         sec = config.get_options(empty_sec)
@@ -48,9 +76,30 @@ class TestConfigWrite(TestConfigWriteBase):
 
 class TestConfigWriteBack(TestConfigWriteBase):
     def test_write(self):
+        opt = self.config.get_option
         t1 = self.factory('temp1')
         self.assertTrue(isinstance(t1, Temp1))
         self.assertEqual(1, t1.aval)
         t1.aval = 5
         self.assertEqual(5, t1.aval)
-        self.assertEqual('5', self.config.get_option('aval', section='temp1'))
+        self.assertEqual('5', opt('aval', section='temp1'))
+        t1.bval = False
+        self.assertEqual('False', opt('bval', section='temp1'))
+        t1.cval = 12.34
+        self.assertEqual('12.34', opt('cval', section='temp1'))
+        t1.dval = 'newval'
+        self.assertEqual('newval', opt('dval', section='temp1'))
+        t1.fval = [6, 7, 8]
+        self.assertEqual('json: [6, 7, 8]', opt('fval', section='temp1'))
+        t1.gval = collections.OrderedDict({'animal': 'cat', 'car': 'fast'})
+        self.assertEqual('json: {"animal": "cat", "car": "fast"}',
+                         opt('gval', section='temp1'))
+
+    def test_write_persistable(self):
+        t2 = self.factory('temp2')
+        self.assertTrue(isinstance(t2, Temp2))
+        self.assertEqual(10, t2.propval)
+        self.assertEqual(2, t2.aval)
+        t2.aval = 5
+        self.assertEqual(5, t2.aval)
+        self.assertEqual('5', self.config.get_option('aval', section='temp2'))
