92f4619c9c80d50a55116aedd962c5bc8df5af9b
Paul Landes <landes@mailc.net>
2020-12-07 17:09:15 -0600

    warnings; correct config

--- src/doc/conf.py
+++ src/doc/conf.py
@@ -58,9 +58,8 @@ extensions = [
 
 # autodoc extension configuration
 autodoc_default_options = {
-# uncomment to enable class level members
-#    'members': True,
-    'special-members': '__init__',
+    # add __init__ methods
+    'special-members': True,
 }
 
 # Add any paths that contain templates here, relative to this directory.
@@ -96,7 +95,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
