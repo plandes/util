ddb1ab5ff56b56f9a7a0c76a573e11171a1c5518
Paul Landes <landes@mailc.net>
2021-02-27 18:15:08 -0600

    doc

--- src/python/zensols/cli/action.py
+++ src/python/zensols/cli/action.py
@@ -37,6 +37,7 @@ class DocUtil(object):
     """
     @staticmethod
     def normalize(text: str) -> str:
+        """Lower case and remove punctuation."""
         doc = text.lower()
         if doc[-1] == '.':
             doc = doc[0:-1]
@@ -114,8 +115,10 @@ class ActionCli(Dictable):
         meths: Dict[str, ActionCliMethod] = {}
         field_params: Set[OptionMetaData] = set()
         f: ClassField
+        # add the dataclass fields that will populate the CLI as options
         for f in self.class_meta.fields.values():
             self._add_option(f.name, field_params)
+        # create an action from each method
         for name in sorted(self.class_meta.methods.keys()):
             meth = self.class_meta.methods[name]
             meth_params = set(field_params)
@@ -128,11 +131,14 @@ class ActionCli(Dictable):
                     if logger.isEnabledFor(logging.DEBUG):
                         logger.debug(f'adding option: {name}:{arg.name}')
                     self._add_option(arg.name, meth_params)
+            # use what's given in the meta, or not present, the munged method
+            # name as the mnemonic
             if self.mnemonics is not None:
                 name = self.mnemonics.get(name)
                 if name is None:
                     continue
             else:
+                # no underscores in the CLI action names
                 name = name.replace('_', '')
             if meth.doc is None:
                 doc = self.class_meta.doc
@@ -160,6 +166,16 @@ class ActionCli(Dictable):
 
 @dataclass
 class ActionCliManager(Dictable):
+    """An :class:`.ActionCli` is created from the configuration given by the
+    section.  Optionally, another section using :obj:`decorator_section_format`
+    will be read to add additional metadata and configuration to instantiated
+    object.  The decorated information is used to help bridge between the class
+    given to be instantiated and the CLI.
+
+    :see: :obj:`actions`
+    :see: :obj:`actions_by_meta_data_name`
+
+    """
     SECTION = 'cli'
     """The application context section."""
 
@@ -170,7 +186,6 @@ class ActionCliManager(Dictable):
     """
 
     config_factory: ConfigFactory = field()
-
     """The configuration factory used to create :class:`.ActionCli` instances.
 
     """
@@ -186,6 +201,10 @@ class ActionCliManager(Dictable):
         return self.config_factory.config
 
     def _create_short_name(self, long_name: str) -> str:
+        """Auto generate  a single letter short option name.
+
+        :param long_name: the name from which to pick a letter
+        """
         for c in long_name:
             if c not in self._short_names:
                 self._short_names.add(c)
@@ -193,6 +212,10 @@ class ActionCliManager(Dictable):
 
     def _create_op_meta_data(self, pmeta: ClassParam,
                              meth: ClassMethod) -> OptionMetaData:
+        """Creates an option meta data used in the CLI from a method parsed from the
+        class's Python source code.
+
+        """
         long_name = pmeta.name.replace('_', '')
         short_name = self._create_short_name(long_name)
         dtype = pmeta.dtype
@@ -213,6 +236,12 @@ class ActionCliManager(Dictable):
             doc=doc)
 
     def _add_field(self, section: str, name: str, omd: OptionMetaData):
+        """Adds the field by name that will later be used in a :class:`.ActionCli`.
+
+        :raises ActionCliManagerError: if ``name`` has already been
+                                       *registered*
+
+        """
         prexist = self._fields.get(name)
         if prexist is not None and omd != prexist:
             raise ActionCliManagerError(
@@ -221,37 +250,62 @@ class ActionCliManager(Dictable):
         self._fields[name] = omd
 
     def _add_action(self, action: ActionCli):
+        """Adds add an action for each method parsed from the action cli Python source
+        code.
+
+        """
         if action.section in self._actions:
             raise ActionCliError(
                 f'duplicate action for section: {action.section}')
+        # for each dataclass field add that field 
         for name, fmd in action.class_meta.fields.items():
             omd = self._create_op_meta_data(fmd, None)
             self._add_field(action.section, fmd.name, omd)
         meth: ClassMethod
+        # add a field for the arguments of each method
         for meth in action.class_meta.methods.values():
             arg: ClassMethodArg
             for arg in meth.args:
                 omd = self._create_op_meta_data(arg, meth)
+                # positional arguments are only referenced in the
+                # ClassInspector parsed source code
                 if not arg.is_positional:
                     self._add_field(action.section, arg.name, omd)
         self._actions[action.section] = action
 
     def _add_app(self, section: str):
+        """Add an :class:`.ActionCli` instanced from the configuration given by a
+        section.  The application is added to :obj:`._actions`.  The section is
+        parsed and use to instantiate an object using
+        :class:`~zensols.config.factory.ImportConfigFactory`.
+
+        Optionally, another section using :obj:`decorator_section_format` will
+        be read to add additional metadata and configuration to instantiated
+        object.  See the class docs.
+
+        :param section: indicates which section to use with config factory
+
+        """
         config = self.config
         class_name: str = config.get_option('class_name', section)
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug(f'building CLI on class: {class_name}')
+        # resolve the string fully qualified class name to a Python class type
         cls = ClassImporter(class_name).get_class()
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug(f'resolved to class: {cls}')
         if not dataclasses.is_dataclass(cls):
             raise ActionCliError('application CLI app must be a dataclass')
+        # parse the source Python code for the class
         inspector = ClassInspector(cls)
         meta: Class = inspector.get_class()
+        # parameters to create the application with the config factory
         params = {'section': section,
                   'class_meta': meta,
                   'options': self._fields}
         conf_sec = self.decorator_section_format.format(**{'section': section})
+        # if we found a decorator action cli config section, use it to set the
+        # configuraiton of the CLI interacts
         if conf_sec in self.config.sections:
             if logger.isEnabledFor(logging.DEBUG):
                 logger.debug(f'found configuration section: {conf_sec}')
@@ -267,6 +321,13 @@ class ActionCliManager(Dictable):
     @property
     @persisted('_actions_pw')
     def actions(self) -> Dict[str, ActionCli]:
+        """Get a list of action CLIs that is used in :class:`.CommandLineParser` to
+        create instances of the application.  Each action CLI has a list of 
+
+        :return: keys are the configuration sections with the action CLIs as
+                 values
+
+        """
         self._short_names: Set[str] = set()
         self._fields: Dict[str, OptionMetaData] = {}
         self._actions: Dict[str, ActionCli] = {}
@@ -282,9 +343,14 @@ class ActionCliManager(Dictable):
 
     @property
     def actions_by_meta_data_name(self) -> Dict[str, ActionCli]:
+        """Return a dict of :class:`.ActionMetaData` instances, each of which is each
+        mnemonic by name and the meta data by values.
+
+        """
         actions = {}
         action: ActionCli
         for action in self.actions.values():
+            meta: Tuple[ActionMetaData]
             for meta in action.meta_datas:
                 if meta.name in actions:
                     raise ActionCliError(f'duplicate meta data: {meta.name}')
