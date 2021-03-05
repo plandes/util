c337c9aef69223fa17053f194206edd2d43ce7fb
Paul Landes <landes@mailc.net>
2021-03-05 07:39:14 -0600

    try to fix race condition in system out echo test

--- test/python/test_executor.py
+++ test/python/test_executor.py
@@ -2,7 +2,8 @@ from io import StringIO
 import sys
 import logging
 from zensols.util import LogConfigurer, Executor
-from logutil import LogTestCase, LogUtil
+from logutil import LogTestCase
+import time
 
 
 class TestExecutor(LogTestCase):
@@ -15,6 +16,7 @@ class TestExecutor(LogTestCase):
         lconf.config_stream(out, err)
         exe = Executor(logger)
         exe.run('echo stdmsg ; echo errmsg > /dev/stderr')
+        time.sleep(.2)
         self.assertEqual('INFO stdmsg\n', out.getvalue())
 
     def run_sleep_out(self):
