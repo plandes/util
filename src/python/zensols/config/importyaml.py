a30b01ba21352cc3abef631c02b95c36e6de595a
Paul Landes <landes@mailc.net>
2022-04-08 09:35:46 -0500

    fix variable interpolation when missing imports

--- src/python/zensols/config/importyaml.py
+++ src/python/zensols/config/importyaml.py
@@ -37,26 +37,30 @@ class ImportYamlConfig(YamlConfig):
                 elif isinstance(c, str):
                     template = _Template(c)
                     rc = template.safe_substitute(context)
+                    if logger.isEnabledFor(logging.DEBUG):
+                        logger.debug(f'subs: {c} -> {rc}')
                     repl[k] = rc
             par.update(repl)
 
         import_def: Dict[str, Any] = self.get_options(
             f'{self.root}.{self.import_name}')
+        cnf: Dict[str, Any] = {}
+        context: Dict[str, str] = {}
+
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug(f'import defs: {import_def}')
+
         if import_def is not None:
-            cnf: Dict[str, Any] = {}
-            context: Dict[str, str] = {}
+            for sec_name, params in import_def.items():
+                config = ConfigurableFactory.from_section(params, sec_name)
+                for sec in config.sections:
+                    cnf[sec] = config.get_options(section=sec)
 
-            if import_def is not None:
-                for sec_name, params in import_def.items():
-                    config = ConfigurableFactory.from_section(params, sec_name)
-                    for sec in config.sections:
-                        cnf[sec] = config.get_options(section=sec)
-
-            self._config.update(cnf)
-            self._flatten(context, '', self._config, ':')
-            new_keys = set(map(lambda k: k.replace(':', '.'), context.keys()))
-            self._all_keys.update(new_keys)
-            repl_node(self._config)
+        self._config.update(cnf)
+        self._flatten(context, '', self._config, ':')
+        new_keys = set(map(lambda k: k.replace(':', '.'), context.keys()))
+        self._all_keys.update(new_keys)
+        repl_node(self._config)
 
     def _compile(self) -> Dict[str, Any]:
         def serialize(par: Dict[str, Any]):
