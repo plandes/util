2de504ea38e1cd8eeeb5f94519e64ce3e89cf0be
Paul Landes <landes@mailc.net>
2023-07-28 13:54:10 -0500

    stdout now logs, captures stdout, and adds path extensions

--- src/python/zensols/util/std.py
+++ src/python/zensols/util/std.py
@@ -53,39 +53,65 @@ class stdout(object):
 
     """
     STANDARD_OUT_PATH: ClassVar[str] = '-'
+    """The string used to indicate to write to standard out."""
 
-    def __init__(self, path: Union[str, Path], *args):
+    def __init__(self, path: Union[str, Path] = None, extension: str = None,
+                 logger: logging.Logger = logger, capture: bool = False,
+                 open_args: str = 'w'):
         """Initailize where to write.  If the path is ``None`` or its name is
         :obj:`STANDARD_OUT_PATH`, then standard out is used instead of opening a
         file.
 
         :param path: the path to write, or ``None``
 
-        :param args: the arguments given to :func:`open`, which defaults to
-                     ``w`` if none are given
+        :param extension: the extension (sans leading dot ``.``) to postpend to
+                          the path if one is not provied in the file name
+
+        :param logger: used to log the successful output of the file, which
+                       defaults to this module's logger
+
+        :param capture: whether to redirect standard out (:obj:`sys.stdout`) to
+                        the file provided by ``path`` if not already indicated
+                        to be standard out
+
+        :param open_args: the arguments given to :func:`open`, which defaults to
+                          ``w`` if none are given
 
         """
         path = Path(path) if isinstance(path, str) else path
         if path is None or path.name == self.STANDARD_OUT_PATH:
-            self._path = None
-            self._args = None
+            self._path: Path = None
+            self._args: str = None
         else:
-            self._path = path
-            self._args = args
-            if len(self._args) == 0:
-                self._args = ['w']
+            if len(path.suffix) == 0 and extension is not None:
+                path = path.parent / f'{path.name}.{extension}'
+            self._path: Path = path
+            self._args: str = open_args
+        self._logger: logging.Logger = logger
+        self._capture: bool = capture
 
     def __enter__(self):
         if self._path is None:
             self._sink = sys.stdout
         else:
-            self._sink = open(self._path, *self._args)
+            self._sink = open(self._path, self._args)
+            if self._capture:
+                self._stdwrite = stdwrite(self._sink)
+                self._stdwrite.__enter__()
         return self._sink
 
     def __exit__(self, type, value, traceback):
         self._sink.flush()
+        logged: bool = False
         if self._sink != sys.stdout:
             try:
                 self._sink.close()
+                logged = value is None and \
+                    self._logger.isEnabledFor(logging.INFO) and \
+                    self._path is not None
             except Exception as e:
                 logger.error(f'Can not close stream: {e}', e)
+        if self._capture:
+            self._stdwrite.__exit__(None, None, None)
+        if logged:
+            self._logger.info(f'wrote: {self._path}')
