2aa0e43395d5628cbd3a0ee5204f78618e9ecabb
Paul Landes <landes@mailc.net>
2024-01-01 10:34:46 -0600

    failure/exception: fix fail pickle, move to separate module

---
+++ src/python/zensols/util/fail.py
@@ -0,0 +1,111 @@
+"""General utility classes to measuring time it takes to do work, to logging
+and fork/exec operations.
+
+"""
+__author__ = 'Paul Landes'
+
+from typing import Dict, Tuple, Any, Type
+from dataclasses import dataclass, field
+import sys
+import traceback
+from collections import OrderedDict
+from io import StringIO, TextIOBase
+
+
+class APIError(Exception):
+    """Base exception from which almost all library raised errors extend.
+
+    """
+    pass
+
+
+@dataclass
+class Failure(object):
+    """Contains information for failures as caught exceptions used by
+    programatic methods.
+
+    """
+    exception: Exception = field(default=None)
+    """The exception that was generated."""
+
+    thrower: Any = field(default=None)
+    """The instance of the class that is throwing the exception."""
+
+    traceback: traceback = field(default=None, repr=False)
+    """The stack trace."""
+
+    message: str = field(default=None)
+    """A high level explaination of what went wrong"""
+
+    def __post_init__(self):
+        self._exc_info: Tuple[Type[Exception], Exception, traceback] = \
+            sys.exc_info()
+        if self.exception is None:
+            self.exception: Exception = self._exc_info[1]
+        if self.traceback is None:
+            self.traceback: traceback = self._exc_info[2]
+        self._traceback_str: str = None
+
+    def _print_stack(self, writer: TextIOBase = sys.stdout):
+        """Print the stack trace of the exception that caused the failure."""
+        traceback.print_exception(*self._exc_info, file=writer)
+
+    @property
+    def traceback_str(self) -> str:
+        """The stack trace as a string.
+
+        :see :meth:`print_stack`
+
+        """
+        if self._traceback_str is None:
+            sio = StringIO()
+            self._print_stack(writer=sio)
+            self._traceback_str = sio.getvalue()
+        return self._traceback_str
+
+    def print_stack(self, writer: TextIOBase = sys.stdout):
+        """Print the stack trace of the exception that caused the failure."""
+        writer.write(self.traceback_str)
+
+    def rethrow(self):
+        """Raises :obj:`exception`."""
+        raise self.exception
+
+    def write(self, depth: int = 0, writer: TextIOBase = sys.stdout):
+        """Write the contents of this instance to ``writer`` using indention
+        ``depth``.
+
+        :param depth: the starting indentation depth
+
+        :param writer: the writer to dump the content of this writable
+
+        """
+        sp = ' ' * (2 * depth)
+        writer.write(f'{sp}{self.message}:\n')
+        self.print_stack(writer)
+
+    def asdict(self) -> Dict[str, Any]:
+        """Return the content of the object as a dictionary."""
+        return OrderedDict(
+            [['message', self.message],
+             ['exception', self.exception],
+             ['trace', self.traceback_str]])
+
+    def __getstate__(self) -> Dict[str, Any]:
+        self.traceback_str
+        state = self.__dict__
+        del state['thrower']
+        del state['traceback']
+        del state['_exc_info']
+        return state
+
+    def __setstate__(self, state: Dict[str, Any]):
+        self.__dict__ = state
+
+    def __str__(self) -> str:
+        msg: str = str(self.exception) if self.message is None else self.message
+        return msg
+
+    def __repr__(self) -> str:
+        msg: str = str(self)
+        return str(f'{type(self.exception).__name__}: {msg}')
---
+++ test/python/test_failure.py
@@ -0,0 +1,70 @@
+import pickle
+import sys
+import traceback
+from io import StringIO, BytesIO
+from logutil import LogTestCase
+from zensols.util import APIError, Failure
+
+
+class TestArgumentParse(LogTestCase):
+    def _raise_ex(self):
+        raise APIError('Test error')
+
+    def _get_failure(self, add_error: bool) -> Failure:
+        try:
+            self._raise_ex()
+        except APIError as e:
+            if add_error:
+                return Failure(e)
+            else:
+                return Failure()
+
+    def _test_failure(self, add_error: bool):
+        fail: Failure = self._get_failure(add_error)
+        self.assertEqual(Failure, type(fail))
+        self.assertEqual('Test error', str(fail))
+        self.assertEqual('APIError: Test error', repr(fail))
+        self.assertTrue(fail.traceback_str.startswith(
+            'Traceback (most recent call last):'))
+        self.assertEqual(7, len(fail.traceback_str.split('\n')))
+
+    def test_raise(self):
+        with self.assertRaisesRegex(APIError, r'^Test error.*'):
+            self._raise_ex()
+
+    def test_failure(self):
+        self._test_failure(True)
+        self._test_failure(False)
+
+    def test_failure_rethrow(self):
+        fail: Failure = self._get_failure(False)
+        ftrace: str
+        try:
+            fail.rethrow()
+        except APIError:
+            sio = StringIO()
+            traceback.print_exception(*sys.exc_info(), file=sio)
+            ftrace = sio.getvalue()
+        self.assertTrue(ftrace.startswith('Traceback (most recent call last):'))
+        self.assertEqual(11, len(ftrace.split('\n')))
+        with self.assertRaisesRegex(APIError, r'^Test error.*'):
+            fail.rethrow()
+
+    def test_failure_pickle(self):
+        fail: Failure = self._get_failure(False)
+        sio = StringIO()
+        fail.write(writer=sio)
+        fwrite: str = sio.getvalue()
+
+        bio = BytesIO()
+        pickle.dump(fail, bio)
+        bio.seek(0)
+        pfail: Failure = pickle.load(bio)
+
+        self.assertEqual(str(fail), str(pfail))
+        self.assertEqual(fail.traceback_str, pfail.traceback_str)
+
+        sio = StringIO()
+        fail.write(writer=sio)
+        pfwrite: str = sio.getvalue()
+        self.assertEqual(fwrite, pfwrite)
