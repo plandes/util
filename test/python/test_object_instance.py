08559f6b699a8d88d9370f9da7b9df4a80fcdb83
Paul Landes <landes@mailc.net>
2024-02-12 19:15:05 -0600

    add app config aliases

--- test/python/test_object_instance.py
+++ test/python/test_object_instance.py
@@ -13,22 +13,22 @@ class Basket(object):
     fruit: Banana
 
 
-# class TestFactoryObjectCreate(unittest.TestCase):
-#     def setUp(self):
-#         config = IniConfig('test-resources/object-factory.conf')
-#         self.fac = ImportConfigFactory(config, shared=False)
+class TestFactoryObjectCreate(unittest.TestCase):
+    def setUp(self):
+        config = IniConfig('test-resources/object-factory.conf')
+        self.fac = ImportConfigFactory(config, shared=False)
 
-#     def _test_banana(self, inst_name: str, should_color: str):
-#         basket = self.fac(inst_name)
-#         self.assertEqual(Basket, basket.__class__)
-#         banana = basket.fruit
-#         self.assertEqual(Banana, banana.__class__)
-#         self.assertEqual(should_color, banana.color)
+    def _test_banana(self, inst_name: str, should_color: str):
+        basket = self.fac(inst_name)
+        self.assertEqual(Basket, basket.__class__)
+        banana = basket.fruit
+        self.assertEqual(Banana, banana.__class__)
+        self.assertEqual(should_color, banana.color)
 
-#     def test_object_create(self):
-#         self._test_banana('basket_instance', 'yellow')
-#         self._test_banana('basket_instance_2', 'brown')
-#         self._test_banana('basket_instance_3', 'green')
+    def test_object_create(self):
+        self._test_banana('basket_instance', 'yellow')
+        self._test_banana('basket_instance_2', 'brown')
+        self._test_banana('basket_instance_3', 'green')
 
 
 class TestAlias(unittest.TestCase):
