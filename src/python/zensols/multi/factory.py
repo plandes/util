108521c5f8540d79ab19097032a63303fb7033e5
Paul Landes <landes@mailc.net>
2025-02-13 03:42:04 -0600

    mult-proc factory stash clears factory now as well

--- src/python/zensols/multi/factory.py
+++ src/python/zensols/multi/factory.py
@@ -118,6 +118,10 @@ class MultiProcessFactoryStash(MultiProcessDefaultStash):
     def _load_from_factory(self, k: str) -> Any:
         return self.factory.load(k)
 
+    def clear(self):
+        super().clear()
+        self.factory.clear()
+
 
 @dataclass(init=False)
 class MultiProcessRobustStash(MultiProcessFactoryStash):
